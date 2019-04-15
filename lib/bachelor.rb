edad034de7c7f4644fe4d0bba2daefdfd03d0429
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 00:59:25 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,6 @@
 def get_first_name_of_season_winner(data, season)
    data[season.to_sym].each do |into_array|
-     if into_array[:status] =="Winner"
+     if into_array[:status] == "Winner"
        return into_array[:name].split(" ")[0]
      end
    end
