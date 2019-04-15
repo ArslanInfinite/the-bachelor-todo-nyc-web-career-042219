232662dafbd41269ae8da98272d9c115e3f2475e
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 01:53:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,3 +16,15 @@ def get_contestant_name(data, occupation)
     end
   end
 end
+
+def count_contestants_by_hometown(data, hometown)
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
+end
