63f2d1dba42592f5a6b1e7a7cd7173ca9514271b
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 01:53:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,18 @@
 require 'pry'
-
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     if contestant["status"] = "Winner"
       return contestant["name"].split[0]
+    end
+  end
+end
+
+def get_contestant_name(data, occupation)
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
     end
   end
 end
